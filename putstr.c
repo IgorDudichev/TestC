f73676222f954b901ef8aae18e2338dcf0b133bc
Igor <igordudichev@gmail.com>
2020-07-20 13:10:57 +0300

    putstr

---
+++ putstr.c
@@ -0,0 +1,23 @@
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putstr(char *str)
+{
+	while (*str != '\0')
+	{
+		ft_putchar(*str);
+		str++;
+	}
+}
+
+int	main(void)
+{
+	char *str;
+	str = "Hello how are you";
+	ft_putstr(str);
+	ft_putchar('\n');
+}
